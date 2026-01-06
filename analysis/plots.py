97afb3f4e2488460b6da039bdca9c52fcda2f23e
manogna09 <148490697+manogna09@users.noreply.github.com>
2026-01-06 23:40:44 +0530

    Update plots.py

--- analysis/plots.py
+++ analysis/plots.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 df = pd.read_csv('../data/olist_orders_dataset.csv', parse_dates=['order_purchase_timestamp'])
 df['month'] = df['order_purchase_timestamp'].dt.to_period('M')
-monthly_sales = df.groupby('month').size()
+monthly_sales = df.groupby('month').sum()
 
 # Plot monthly order count
 monthly_sales.plot(kind='bar', figsize=(12, 6), title='Monthly Order Volume')
